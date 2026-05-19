6b291c51f792f6847065d3cf28825c5d47f7de7a
shehnazjavaid55@gmail.com <87583086+ShehnazJawed@users.noreply.github.com>
2026-05-19 12:28:55 +0500

    Jwt Tokem Configuration and customize Source code

--- SpringSecurity-App/SpringSecurity-App/src/main/java/com/shehnaz/controller/UserController.java
+++ SpringSecurity-App/SpringSecurity-App/src/main/java/com/shehnaz/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.shehnaz.controller;
 
+import com.shehnaz.Service.JwtService;
 import com.shehnaz.Service.UserService;
 import com.shehnaz.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,10 +18,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     @PostMapping("/register")
     public User registerUser(@RequestBody User user){
-
         userService.saveUser(user);
         return user;
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+        Authentication authentication= authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getUsername(),user.getPassword()));
+        if(authentication.isAuthenticated())
+            return jwtService.generateToken(user.getUsername());
+        else
+            return "Login Failed";
+
     }
 }
